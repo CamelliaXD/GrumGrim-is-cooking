ced4789c2d60d738c1594bc5e5acd9b893b4e7ce
CamelliaXD <144575056+CamelliaXD@users.noreply.github.com>
2025-03-08 16:08:40 +0700

    update

--- Assets/Scripts/Main/buttom.cs
+++ Assets/Scripts/Main/buttom.cs
@@ -2,17 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
 public class buttom : MonoBehaviour
 {
 
     [SerializeField] AudioSource clickSFX;
     public void Buttom()
     {
-        SceneManager.LoadScene("Menu");
+        StartCoroutine(DelayBeforeLoadScene("Menu"));
     }
-    public void Quit()
+    public void Setting()
+    {
+        StartCoroutine(DelayBeforeLoadScene("Setting"));
+    }
+    public void Quits()
     {
         Application.Quit();
     }
-    
+
+    IEnumerator DelayBeforeLoadScene(string sceneName)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        if (clickSFX != null)
+        {
+            clickSFX.Play();
+        }
+
+        yield return new WaitForSeconds(0.5f); 
+
+        SceneManager.LoadScene(sceneName);
+    }
+ 
 }
--- Assets/Scripts/Menu/buttomongame1.cs
+++ Assets/Scripts/Menu/buttomongame1.cs
@@ -2,16 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 public class buttomongame1 : MonoBehaviour
 {
     [SerializeField] AudioSource clickSFX;
     public void Button()
     {
-        SceneManager.LoadScene("SampleScene");
+        StartCoroutine(DelayBeforeLoadScene("SampleScene"));
     }
     public void back()
     {
-        SceneManager.LoadScene("Main");
+        StartCoroutine(DelayBeforeLoadScene("Main"));
     }
+    IEnumerator DelayBeforeLoadScene(string sceneName)
+    {
+        EventSystem.current.SetSelectedGameObject(null);
 
+        if (clickSFX != null)
+        {
+            clickSFX.Play();
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        SceneManager.LoadScene(sceneName);
+    }
 }
