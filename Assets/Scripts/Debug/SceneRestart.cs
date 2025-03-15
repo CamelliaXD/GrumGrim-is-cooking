191ab9191742a737408d9bfa7b7aec5af4a200f7
CamelliaXD <144575056+CamelliaXD@users.noreply.github.com>
2025-03-15 14:04:56 +0700

    update

--- Assets/Scripts/Debug/SceneRestart.cs
+++ Assets/Scripts/Debug/SceneRestart.cs
@@ -1,17 +1,18 @@
 using UnityEngine;
 using UnityEditor;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
+
 
 namespace Editor
 {
-    public class SceneRestart : MonoBehaviour
-    {
-        [MenuItem("RestartScene/Restart Scene #R")]
-        private static void RestartScene()
-        {
-            var currentScene = EditorSceneManager.GetActiveScene();
-            EditorSceneManager.LoadScene(currentScene.name);
-        }
-    }
+    //public class SceneRestart : MonoBehaviour
+    //{
+        //[MenuItem("RestartScene/Restart Scene #R")]
+        //private static void RestartScene()
+        //{
+           // var currentScene = EditorSceneManager.GetActiveScene();
+           // EditorSceneManager.LoadScene(currentScene.name);
+        //}
+    //}
 }
 
